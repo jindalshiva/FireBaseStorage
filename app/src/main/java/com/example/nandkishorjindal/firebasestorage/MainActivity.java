03b28a654d585f5eb691fde318311ba75dfd4e2d
nandkishorjindal <jindalshivani11@gmail.com>
2018-10-09 16:34:13 +0530

    Fourth commit

--- app/src/main/java/com/example/nandkishorjindal/firebasestorage/MainActivity.java
+++ app/src/main/java/com/example/nandkishorjindal/firebasestorage/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.nandkishorjindal.firebasestorage;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonGallery, buttonCamera,buttonUpdate;
+    private ImageView imageView;
+    private Uri file;
+    private StorageReference mStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCamera = (Button) findViewById(R.id.button_photo);
+        buttonGallery = (Button) findViewById(R.id.button_Gallery);
+        imageView = (ImageView) findViewById(R.id.imageview);
+        buttonUpdate = (Button) findViewById(R.id.button_update);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        buttonGallery.setOnClickListener(this);
+        buttonCamera.setOnClickListener(this);
+        buttonUpdate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
